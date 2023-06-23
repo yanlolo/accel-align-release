2f6954776942e9487909ebe1e9231e3db8a365b7
elias.bichler <elias_bichler@gmx.at>
2023-06-23 15:43:04 +0200

    update project

--- accalign.cpp
+++ accalign.cpp
@@ -823,7 +823,9 @@ void AccAlign::find_candidate_positions_using_strobealign(std::string_view seq, 
   if (nams.empty() || nonrepetitive_fraction < 0.7) {
     nams = find_nams_rescue(query_randstrobes, index, map_params.rescue_cutoff);
   }
-  std::sort(nams.begin(), nams.end(), score);
+  std::sort(nams.begin(), nams.end(), [](const Nam &a, const Nam &b) -> bool {
+    return a.score > b.score;
+  });
 
   Region region;
   region.matched_intervals.reserve(1);
